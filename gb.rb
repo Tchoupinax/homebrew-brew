ea7cc3f804c1c007d0322602002f1197658b4e32
Corentin <corentin.filoche@swan.io>
2022-03-15 23:41:56 +0100

    feat: upgrade gb to v0.0.6

--- gb.rb
+++ gb.rb
@@ -1,8 +1,8 @@
 class Gb < Formula
   desc "Tools to easily switch and view your git branches"
   homepage "https://github.com/Tchoupinax/git-branch"
-  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "fe6883a9779b92261c58229b8da4c7acf16119c424aed6128812da53dc7fcf73"
+  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "ca827aaa2dfea10a4fc16f5c08ca663b7d7d65a86aadd85ac34f2d1b75ce51a5"
   license "MIT"
 
   depends_on "go" => :build
