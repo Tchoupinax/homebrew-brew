81a94cd18823eae9d02bfb3057c26fafecf5e7dc
Tchoupinax <corentinfiloche@hotmail.fr>
2022-04-02 13:35:55 +0200

    feat: upgrade gb to v0.0.8

--- gb.rb
+++ gb.rb
@@ -1,8 +1,8 @@
 class Gb < Formula
   desc "Tools to easily switch and view your git branches"
   homepage "https://github.com/Tchoupinax/git-branch"
-  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "b6504de3879277a575aac54ea713c3aec00b305d464e4f1694ae646451ec05e5"
+  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "b17a4c44f002f6f1e80aea4e8c0e9ac5261425025a1f286d7693b5eb8097f834"
   license "MIT"
 
   depends_on "go" => :build
