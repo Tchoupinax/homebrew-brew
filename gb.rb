1bbb04196abab4aeff646ed9990ce49836ff3ece
Tchoupinax <corentinfiloche@hotmail.fr>
2023-11-26 12:08:58 +0100

    feat: upgrade gb to v0.0.11

--- gb.rb
+++ gb.rb
@@ -1,8 +1,8 @@
 class Gb < Formula
   desc "Tools to easily switch and view your git branches"
   homepage "https://github.com/Tchoupinax/git-branch"
-  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.10.tar.gz"
-  sha256 "d0cd2cf2494b27f0a07e97d7e1d803de1ff0388f9a918694e534384fe75f1ac2"
+  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.11.tar.gz"
+  sha256 "3b1a1106156c92ab4ef8dd29916e3e7bff6b2ff916b6dc54893738fe913eefe0"
   license "MIT"
 
   depends_on "go" => :build
