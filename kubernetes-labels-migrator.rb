3d48519f212112f0950d0d4791522324a8be9256
Tchoupinax <corentinfiloche@hotmail.fr>
2024-03-24 23:19:14 +0100

    chore: prepare release of kubernetes-labels-migrator

---
+++ kubernetes-labels-migrator.rb
@@ -0,0 +1,17 @@
+class Gb < Formula
+  desc "Your favorite tool to migrate labels on your deploy without any downtime "
+  homepage "https://github.com/Tchoupinax/k8s-labels-migrator"
+  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.11.tar.gz"
+  sha256 "3b1a1106156c92ab4ef8dd29916e3e7bff6b2ff916b6dc54893738fe913eefe0"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    system "false"
+  end
+end
