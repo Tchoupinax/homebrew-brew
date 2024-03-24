621624b77eee69d3a96547772a1bbb771459d71c
Tchoupinax <corentinfiloche@hotmail.fr>
2024-03-24 23:43:32 +0100

    chore: rename

--- kubernetes-labels-migrator.rb
+++ kubernetes-labels-migrator.rb
@@ -1,9 +1,9 @@
-class Gb < Formula
-  desc "Your favorite tool to migrate labels on your deploy without any downtime"
+class KubernetesLabelsMigrator < Formula
+  desc "Your favorite tool to migrate labels on your deploy without any downtime "
   homepage "https://github.com/Tchoupinax/k8s-labels-migrator"
-  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.11.tar.gz"
-  sha256 "3b1a1106156c92ab4ef8dd29916e3e7bff6b2ff916b6dc54893738fe913eefe0"
-  license "MIT"
+  url "https://github.com/Tchoupinax/k8s-labels-migrator/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "226f70f74e488d4c31969c21915dc34c302a950b0dfc4a0d5ffc011b3cfab4f7"
+  license "GPL-3.0"
 
   depends_on "go" => :build
 
