c9a1f4911b9a1e82837f9657649e7d08c50d71b6
Tchoupinax <corentinfiloche@hotmail.fr>
2024-09-24 15:37:39 +0200

    feat(k8s-labels-migrator): publish version 0.1.5

--- kubernetes-labels-migrator.rb
+++ kubernetes-labels-migrator.rb
@@ -1,8 +1,8 @@
 class KubernetesLabelsMigrator < Formula
-  desc "Your favorite tool to migrate labels on your deployments without any downtime "
+  desc "Your favorite tool to migrate labels on your deployments without any downtime."
   homepage "https://github.com/Tchoupinax/k8s-labels-migrator"
-  url "https://github.com/Tchoupinax/k8s-labels-migrator/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "89b0f33cf47799c0bf9cbe8ebc33a34015d5bbcda00b1e4fcd594dda37da1fbd"
+  url "https://github.com/Tchoupinax/k8s-labels-migrator/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "8dc92f20783b155dc6e31e9bd24559524ce78d6058d996efdee79dc5866b126f"
   license "GPL-3.0"
 
   depends_on "go" => :build
