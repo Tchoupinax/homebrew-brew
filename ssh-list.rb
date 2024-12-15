269fdf783e7563a406392113a97bd534fa8f4557
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-15 20:46:40 +0100

    feat(ssh-list): provide more metadata

--- ssh-list.rb
+++ ssh-list.rb
@@ -1,6 +1,6 @@
 class SshList < Formula
-  desc "_"
-  homepage "_"
+  desc "Cross platform tools to display your registered ssh connections "
+  homepage "https://github.com/Tchoupinax/ssh-list"
   url "https://github.com/Tchoupinax/ssh-list/archive/refs/tags/v0.3.2.tar.gz"
   sha256 "e41f0ada92760cc08a3c9c53ceea13d1c11b7625d23a0ca3dc37d8c5a65e72cb"
   license "MIT"
