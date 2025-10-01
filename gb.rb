96648c96b3f5929d2ab893ee9eb6b003149d053e
Tchoupinax <corentinfiloche@hotmail.fr>
2025-10-01 11:34:28 +0200

    feat(git-branch): publish version 0.4.0

--- gb.rb
+++ gb.rb
@@ -1,8 +1,8 @@
 class Gb < Formula
   desc "Tools to easily switch and view your git branches"
   homepage "https://github.com/Tchoupinax/git-branch"
-  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "6e21f50ac2e46f65fcdf2ead297a4ee9f84990a49db756004ad4e5d6d2187c6d"
+  url "https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "88760a26df25eb6d41a6711318ef8fe0d6dc7d485d633745ee58814d3424cd8d"
   license "MIT"
 
   depends_on "go" => :build
