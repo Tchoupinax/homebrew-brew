7a4c2b6b0d76b8ac77f777f00d422ded2bad6a8d
Tchoupinax <corentinfiloche@hotmail.fr>
2026-04-10 20:43:23 +0200

    chore(package-json-formatter): refactor install script

--- pjf.rb
+++ pjf.rb
@@ -8,7 +8,28 @@ class Pjf < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    require "net/http"
+    require 'date'
+
+    uri = URI("https://api.github.com/repos/Tchoupinax/package-json-formatter/releases/latest")
+    resp = Net::HTTP.get(uri)
+    resp_json = JSON.parse(resp)
+    
+    latest_version = resp_json["tag_name"]
+    build_date = Date.parse(resp_json["published_at"]).strftime('%Y-%m-%d')
+
+    commit_uri = URI("https://api.github.com/repos/Tchoupinax/package-json-formatter/commits/#{latest_version}")
+    commit_resp = Net::HTTP.get(commit_uri)
+    commit_hash = JSON.parse(commit_resp)["sha"]
+
+    ldflags = %W[
+      -s -w
+      -X pjf/cmd.version=#{latest_version}
+      -X pjf/cmd.buildDate=#{build_date}
+      -X pjf/cmd.commit=#{commit_hash}
+    ]
+
+    system "go", "build", "-mod=readonly", *std_go_args(ldflags:), "./cmd/pjf"
   end
 
   test do
